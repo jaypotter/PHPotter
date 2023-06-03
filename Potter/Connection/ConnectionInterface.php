d93ff48b10df3d68995dacaa0a0dd21931246558
Jay Potter <j@ypotter.ca>
2023-06-03 12:18:56 -0400

    connect and disconnect methods

--- Potter/Connection/ConnectionInterface.php
+++ Potter/Connection/ConnectionInterface.php
@@ -5,4 +5,10 @@ namespace Potter\Connection;
 interface ConnectionInterface
 {
     
+    public function connect(): void;
+    
+    public function connected(): bool;
+    
+    public function disconnect(): void;
+    
 }
